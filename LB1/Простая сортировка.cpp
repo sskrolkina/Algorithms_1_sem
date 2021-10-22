3d46c92ed6eac33207a7200d45a2e27c0516e715
Snezhana <89837216+sskrolkina@users.noreply.github.com>
2021-10-23 00:14:47 +0300

    Add files via upload

---
+++ LB1/Простая сортировка.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin("smallsort.in");
+    ofstream fout("smallsort.out");
+
+    long long a[10000], n;
+    fin >> n;
+    for (int i = 0; i < n; i++) {
+        fin >> a[i];
+    }for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }for (int i = 0; i < n; i++) {
+        fout << a[i] << " ";
+    }
+
+    return 0;
+}
