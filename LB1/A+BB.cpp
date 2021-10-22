55c523023b2917fc446bfdd0a9fc6e697522fec4
Snezhana <89837216+sskrolkina@users.noreply.github.com>
2021-10-23 00:11:52 +0300

    Add files via upload

---
+++ LB1/A+BB.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    long long a, b, c;
+    ifstream fin("aplusbb.in");
+    ofstream fout("aplusbb.out");
+
+
+    fin >> a >> b;
+    fout << a + b * b;
+
+    return 0;
+}
