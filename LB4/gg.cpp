1474b31b6a776ed9c7679e543ffed1f99afed104
Snezhana <89837216+sskrolkina@users.noreply.github.com>
2021-11-24 10:43:19 +0300

    Add files via upload

---
+++ LB4/gg.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin("queue.in");
+    ofstream fout("queue.out");
+    queue<int> happy;
+    int n;
+    fin >> n;
+    char znak;
+
+    for (int i = 0; i < n; i++) {
+        fin >> znak;
+        if (znak == '+') {
+            int b;
+            fin >> b;
+            happy.push(b);
+        } else {
+            fout << happy.front() << endl;
+            happy.pop();
+        }
+    }
+    return 0;
+}
